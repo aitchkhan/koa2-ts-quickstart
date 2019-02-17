fa75c172959a81d3cd49cbef5a97bc248fbb2e61
aitchkhan <aitchkhan@gmail.com>
2019-02-17 10:57:16 +0500

    fix apidoc mount issue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import 'reflect-metadata';
 import { Container } from 'typedi';
 import { createConnection, useContainer } from 'typeorm';
@@ -8,31 +9,30 @@ import * as Koa from 'koa';
 import * as bodyparserMiddleware from 'koa-bodyparser';
 import * as loggerMiddleware from 'koa-bunyan-logger';
 import * as jsonMiddleware from 'koa-json';
+import * as mount from 'koa-mount';
 import * as serve from 'koa-static';
 
 import errorMiddleware from './middleware/error';
 import requestMiddleware from './middleware/request';
 
 import routeMiddleware from './route';
-import * as path from 'path';
 
 import conf from './conf';
 
 const app = new Koa();
 const d = debug('kickstarter:root');
-const apidocPath = path.resolve(__dirname, '..', 'apidoc');
-console.log(apidocPath);
+const apidocPath = path.resolve(__dirname + '..' + '/apidoc/');
 
 // Register middleware
 app.use(bodyparserMiddleware());
 app.use(jsonMiddleware());
 app.use(loggerMiddleware());
 app.use(requestMiddleware());
-app.use(serve(apidocPath));
 app.use(errorMiddleware());
 
 // Registers routes via middleware
 app.use(routeMiddleware());
+app.use(mount('/apidoc', serve(__dirname + '/apidoc/')));
 
 d('current environment: %s', conf.get('env'));
 d('server started at port: %d', conf.get('port'));
@@ -43,6 +43,6 @@ createConnection().then((connection: any) => {
   d('db connection established');
   app.listen(conf.get('port'));
 }).catch((e: Error) => {
-  console.log('db connection failed', e);
+  logg('db connection failed', e);
   process.exit(0);
 });
